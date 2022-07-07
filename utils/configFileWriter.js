2f51e93bbb01449d963a7a7ebf251aaff25ba31f
jonathan <jonathan@breeze-technologies.de>
2022-07-07 17:29:12 +0200

    Rewrote config writer to limit amount of file writing

--- utils/configFileWriter.js
+++ utils/configFileWriter.js
@@ -22,26 +22,34 @@ if (fs.existsSync(filePath)) {
     const sensorConfigFromLocalJSON = fs.readFileSync(filePath);
     const sensorConfigParsed = JSON.parse(sensorConfigFromLocalJSON);
 
-    // Grab the dates as milliseconds so we can compare if the document fetched from MongoDB is more recent than our local file
+    // Convert the dates into milliseconds so we can compare if the document fetched from MongoDB is more recent than our local file
     const dateFromMongo = latestSensorConfig.createdAt.getTime();
     const dateFromLocalJSON = new Date(sensorConfigParsed.createdAt).getTime();
     console.log('Mongo date is more recent? --> ', dateFromMongo > dateFromLocalJSON);
 
-    if (dateFromMongo > dateFromLocalJSON)
+    if (dateFromMongo > dateFromLocalJSON) {
         // If the Mongo document is newer, we (over)write the file locally to the repository
-        try {
-            fs.writeFileSync(filePath, JSON.stringify(latestSensorConfig, null, 4), 'utf8');
-            console.log(`File was saved to ${filePath}`);
-        } catch (err) {
-            console.err('An error has ocurred while saving the file.');
-        }
-} else {
-    try {
-        fs.writeFileSync(filePath, JSON.stringify(latestSensorConfig, null, 4), 'utf8');
-        console.log(`File was saved to ${filePath}`);
-    } catch (err) {
-        console.err('An error has ocurred while saving the file.');
+        fs.writeFile(filePath, JSON.stringify(latestSensorConfig, null, 4), 'utf8', function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        console.log(`File ${filePath} was overwritten with a newer JSON document`);
     }
+} else if (!fs.existsSync(filePath)) {
+    fs.writeFile(filePath, JSON.stringify(latestSensorConfig, null, 4), 'utf8', function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+    console.log(`A new JSON document was saved to ${filePath}`);
 }
+
+// try {
+//     fs.writeFileSync(filePath, JSON.stringify(latestSensorConfig, null, 4), 'utf8');
+//     console.log(`File was saved to ${filePath}`);
+// } catch (err) {
+//     console.err('An error has ocurred while saving the file.');
+// }
 
 mongoose.connection.close();
