53ce7dc06cb55ac77c895753305e1a12460cf6b4
jonathan <jonathan@breeze-technologies.de>
2022-07-27 14:32:23 +0200

    Updated caching query

--- caching/cacheMiddleware.js
+++ caching/cacheMiddleware.js
@@ -1,16 +1,19 @@
 import NodeCache from 'node-cache';
 import SensorConfigs from '../models/SensorConfigs.js';
 
-const cache = new NodeCache({ stdTTL: 5 * 60 });
+const cache = new NodeCache({ stdTTL: 3 });
 
 export const cacheQuery = async (req, res, next) => {
     try {
         if (cache.has('sensorconfigfile')) {
-            return res.send(cache.get('sensorconfigfile'));
+            const getConfigFile = cache.get('sensorconfigfile');
+            console.log('HIT --> ', JSON.parse(getConfigFile));
+            return res.send(getConfigFile);
         } else {
             const sensorConfig = await SensorConfigs.findOne().sort({ $natural: -1 });
-            cache.set('sensorconfigfile', JSON.stringify(sensorConfig), 300);
-            res.send(sensorConfig);
+            cache.set('sensorconfigfile', JSON.stringify(sensorConfig), 3);
+            console.log('MISS');
+            res.json(sensorConfig);
         }
     } catch (error) {
         next(error);
